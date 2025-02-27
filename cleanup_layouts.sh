dae75e05eaeab9ea146270c8522e85ee257420f4
MZed Admin <admin@mzed.com>
2025-02-27 13:18:20 -0600

    html to markdown extension

--- cleanup_layouts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove the redundant courses.html layout file
-echo "Removing redundant courses.html layout file..."
-rm -f _layouts/courses.html
-
-echo "Layout cleanup completed successfully!" 
